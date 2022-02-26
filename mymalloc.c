8ba1fad451229319b9f7f1b16f00c6478166209f
clum1124 <connorl343@gmail.com>
2022-02-26 16:36:13 -0500

    Malloc

--- mymalloc.c
+++ mymalloc.c
@@ -8,6 +8,7 @@
 #define META_LENGTH 5
 #define NOT_ALLOC '0'
 #define ALLOC '1'
+#define SMAlLEST_FREE 4
 #define TRUE 1
 #define FALSE 0
 
@@ -23,17 +24,21 @@ void *mymalloc(size_t size, char *file, int line) {
 
     // find a free chunk
     int index = 0;
+    int createNewBlock = FALSE;
     while (TRUE) {
 
         if (index >= MEM_SIZE)
             return NULL;
 
         char curr_alloc_stat = memory[index];
-        int curr_alloc_size = memory[index+1];
+        size_t curr_alloc_size = memory[index+1];
 
-        if (curr_alloc_stat == NOT_ALLOC && curr_alloc_size >= size + META_LENGTH)
+        if (curr_alloc_stat == NOT_ALLOC && curr_alloc_size >= size) {
+        	if (curr_alloc_size >= size + META_LENGTH + SMALLEST_FREE) {
+        		createNewBlock = TRUE;
+        	}
             break;
-
+        }
         else {
             index = index + META_LENGTH + curr_alloc_size;
         }
@@ -42,19 +47,28 @@ void *mymalloc(size_t size, char *file, int line) {
 
     // allocate a chunk of memory
     // sets up the metadata for the next block
-    size_t curr_size = memory[index + STAT_SIZE];
-    memory[index + META_LENGTH + size] = NOT_ALLOC;
-    memory[index + META_LENGTH + size + STAT_LENGTH] = curr_size - size;
+    if (createNewBlock) {
+    	size_t curr_size = memory[index + STAT_SIZE];
+    	memory[index + META_LENGTH + size] = NOT_ALLOC;
+    	memory[index + META_LENGTH + size + STAT_LENGTH] = curr_size - size;
 
-    // adjusts the metadata of the block of memory to be returned
-    memory[index] = ALLOC;
-    memory[index+1] = size;
+    	// adjusts the metadata of the block of memory to be returned
+    	memory[index] = ALLOC;
+    	memory[index+1] = size;
+    }
+    else
+    	memory[index] = ALLOC;
+
     int *p = &memory[index + META_LENGTH];
+    	return p;
+
+
+
 
     return p;
 }
 
 void myfree(void *ptr, char *file, int line)
 {
-    ...
+	...
 }
